33a2cf016a7ed28de163819f9f83a18023f06ffc
UmActually <a00832792@tec.mx>
2025-08-02 01:10:45 -0600

    added links, photo gallery, semitones landing pages and redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,23 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://umm-actually.com"
+    site: "https://umm-actually.com",
+    redirects: {
+        "/linkedin": {
+            status: 301,
+            destination: "https://www.linkedin.com/in/leonardo-corona-garza-8b4847269/"
+        },
+        "/ajedrez": {
+            status: 301,
+            destination: "https://open.spotify.com/track/09pjlooexSLZuAgaylvmCA"
+        },
+        "/semitones": {
+            status: 301,
+            destination: "https://apps.apple.com/us/app/semitones-chromatic-tuner/id6740186119"
+        },
+        "/rola-del-clima": {
+            status: 301,
+            destination: "https://youtube.com/watch?v=LKXrcDCfV-c"
+        }
+    }
 });
--- src/data/types.ts
+++ src/data/types.ts
@@ -1,14 +1,19 @@
-export interface JobDetails {
+export interface ProjectJobDetails {
     position: string;
     company: string;
     city: string;
+}
+
+export interface ProjectLink {
+    label: string;
+    url: string;
 }
 
 export interface Project {
     id: string;
     name: string;
     tldr: string;
-    job: JobDetails | null;
+    job: ProjectJobDetails | null;
     date: string;
     shortDescription: string;
     detailedDescription: string | null;
@@ -16,5 +21,7 @@ export interface Project {
     role: string;
     technologies: string[];
     skills: string[];
+    links: ProjectLink[];
     iconURL: string | null;
+    gallery: string[];
 }
